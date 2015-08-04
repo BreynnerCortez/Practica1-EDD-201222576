0cd055ce042eccd8245df54353d4a3b366fcac15
Breynner <Breynner@10.56.81.42>
2015-08-04 11:03:09 -0600

    #5 Retomando Labores

---
+++ Practica1-EDD-201222576/src/practica1/edd/pkg201222576/Hola.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1.edd.pkg201222576;
+
+/**
+ *
+ * @author Breynner
+ */
+public class Hola {
+    public Hola(){
+        
+    }
+}
